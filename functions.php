d20101a5baa363f85ab82d6865e357143d842b5b
Stephanie Quibel <nanou1972@msn.com>
2023-10-01 20:47:11 +0200

    intégration de theme-setup.php

--- functions.php
+++ functions.php
@@ -1,8 +1,4 @@
 <?php
-// Ajouter la prise en charge des images mises en avant
-add_theme_support('post-thumbnails');
-// Ajouter automatiquement le titre du site dans l'en-tête du site
-add_theme_support('title-tag');
 
 function mota_register_assets()
 {
@@ -12,8 +8,8 @@ function mota_register_assets()
 add_action('wp_enqueue_scripts', 'mota_register_assets');
 
 
-//insertion de l'onglet menu dans l'admin
-register_nav_menus(array(
-    'nav_menu' => 'Menu principal',
-    'footer_menu' => 'Bas de page'
-));
+
+/**
+ * Menus/Theme options definitions
+ */
+require_once 'includes/theme-setup.php';
