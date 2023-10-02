63c99b2014d16df6968d3db5108d086c0d8a5b92
Stephanie Quibel <nanou1972@msn.com>
2023-10-02 17:33:02 +0200

    création de la modal

---
+++ template-parts/site-modal-contact.php
@@ -0,0 +1,6 @@
+<div class="site_modal_contact">
+    <div class="site_modal_header">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-header.svg" alt="Image du mot contact répété plusieurs fois en en-tête">
+    </div>
+    <?php echo do_shortcode('[contact-form-7 id="63f3a8f" title="contact modal"]'); ?>
+</div>
