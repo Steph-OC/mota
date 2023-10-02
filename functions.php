da095dc82ccd0ad5b530e98846608283e6bf10e7
Stephanie Quibel <nanou1972@msn.com>
2023-10-02 17:44:24 +0200

    ajout du fichier form-delete-p.php

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function mota_register_assets()
 {
     wp_enqueue_style('mota', get_stylesheet_uri(), array(), '1.0');
     wp_enqueue_style('fonts-style',  get_stylesheet_directory_uri() . '/assets/css/fonts-style.css');
+    wp_enqueue_script('script', get_stylesheet_directory_uri() . '/assets/js/script.js', array(), 1.1, true);
 }
 add_action('wp_enqueue_scripts', 'mota_register_assets');
 
@@ -13,3 +14,7 @@ add_action('wp_enqueue_scripts', 'mota_register_assets');
  * Menus/Theme options definitions
  */
 require_once 'includes/theme-setup.php';
+/**
+ * Remove auto p from Contact Form 7 shortcode output
+ */
+require_once 'includes/form-delete-p.php';
