736797ba2e50e4f8e9bae66f6a7574b60f661334
Stephanie Quibel <nanou1972@msn.com>
2023-10-04 23:19:06 +0200

    test animation out modal

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+window.onload = (event) => {
+    const openModal = document.querySelector('.openModal');
+    const siteModal = document.querySelector('.site_modal_contact');
+    const siteOverlay = document.querySelector('.mask');
+    
+    
+    openModal.addEventListener('click', (e) => {
+        siteModal.style.display = 'block';
+        siteOverlay.style.display = 'block';
+        e.preventDefault();
+    });
+    
+    siteOverlay.addEventListener('click', (event) => {
+        if(event.target == siteOverlay) {
+           siteModal.style.display= 'none';
+            siteOverlay.style.display = 'none';
+        }
+    });
+    }
