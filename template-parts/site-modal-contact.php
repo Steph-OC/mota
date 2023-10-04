34b1cd19c52102c161c1e37010589d37e0c9841f
Stephanie Quibel <nanou1972@msn.com>
2023-10-04 12:09:19 +0200

    création d'un template part pour la modal

--- template-parts/site-modal-contact.php
+++ template-parts/site-modal-contact.php
@@ -1,6 +1,10 @@
+<div class="mask">
 <div class="site_modal_contact">
     <div class="site_modal_header">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-header.svg" alt="Image du mot contact répété plusieurs fois en en-tête">
     </div>
-    <?php echo do_shortcode('[contact-form-7 id="63f3a8f" title="contact modal"]'); ?>
+    <div class="site_modal_content">
+        <?php echo do_shortcode('[contact-form-7 id="63f3a8f" title="contact modal"]'); ?>
+    </div>
+</div>
 </div>
