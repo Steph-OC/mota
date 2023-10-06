8fed15cd42aa22570320606311a7e2f3b0153a01
Stephanie Quibel <nanou1972@msn.com>
2023-10-06 18:34:43 +0200

    finalisation de la modal fade in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 window.onload = (event) => {
     const openModal = document.querySelector('.openModal');
-    // Get the modal
     const modal = document.getElementById('myModal');
 
-    // When the user clicks on the button, open the modal
+
     openModal.addEventListener('click', (e) => {
         modal.classList.add('show');
         e.preventDefault();
     });
-    // When the user clicks anywhere outside of the modal, close it
+
     window.addEventListener('click', (event) => {
         if (event.target == modal) {
             modal.classList.remove('show');
