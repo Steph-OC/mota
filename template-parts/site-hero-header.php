98bb0aca627b6cee2260d1fcc4e6e81fdc6c6e2f
Stephanie Quibel <nanou1972@msn.com>
2023-10-29 00:54:30 +0200

    hero header avec image aléatoire

--- template-parts/site-hero-header.php
+++ template-parts/site-hero-header.php
@@ -1,4 +1,21 @@
-<div class="site_hero_header">
-    <img class="site_hero_image" src="<?php echo get_template_directory_uri(); ?>/assets/images/nathalie-9-1.webp" alt="Photo où l'on voit une mariée avec d'autres personnes dansant en ligne">
-    <h1>PHOTOGRAPHE EVENT</h1>
-</div>
+<?php
+
+$args = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 1,
+    'orderby' => 'rand',
+);
+
+$my_query = new WP_Query($args);
+if ($my_query->have_posts()) :
+    while ($my_query->have_posts()) : $my_query->the_post(); ?>
+        <div class="site_hero_header">
+            <?php the_post_thumbnail('large', array('class' => 'site_hero_image')); ?>
+            <h1>PHOTOGRAPHE EVENT</h1>
+        </div>
+<?php
+    endwhile;
+endif;
+wp_reset_postdata();
+
+?>
